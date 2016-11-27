682c822a055ec83d1566d310b7c15122d58de24a
JEONG HYUN SEOK <nicewook@hotmail.com>
2016-11-27 16:32:53 +0900

    Update 2.2_jhs_k_th_from_last_linkedlist.py

--- Craking the Coding Interview/2.2_jhs_k_th_from_last_linkedlist.py
+++ Craking the Coding Interview/2.2_jhs_k_th_from_last_linkedlist.py
@@ -1,3 +1,4 @@
+__author__ = "JHS"
 
 import unittest
 
@@ -16,41 +17,40 @@ class LinkedList:
             cur = cur.next
         cur.next = Node(item)
 
-    if __name__ == '__main__':
-        def printlist(self):
-            cur = self.head
-            res = []
-            while cur is not None:
-                res.append(cur.val)
-                cur = cur.next
-            return str(res)
+    def printlist(self):
+        cur = self.head
+        res = []
+        while cur is not None:
+            res.append(cur.val)
+            cur = cur.next
+        return str(res)
 
-        #여기까진 일반적 링크드리스트이다
+    #여기까진 일반적 링크드리스트이다
 
-        def kth_element_from_last(selfself,k):
-            p1 = self.head
-            p2 = self.head
+    def kth_element_from_last(selfself,k):
+        p1 = self.head
+        p2 = self.head
 
-            if k != 0:
-                # p2를 k만큼 이동시킨다. 그러면 p1, p2의 간격은 k만큼이 된다.
-                if __name__ == '__main__':
-                    for i in range(k):
-                        p2 = p2.next
+        if k != 0:
+            # p2를 k만큼 이동시킨다. 그러면 p1, p2의 간격은 k만큼이 된다.
+            if __name__ == '__main__':
+                for i in range(k):
+                    p2 = p2.next
 
-                    # 전체 링크드리스트의 길이보다 k가 더 길면 노답인거다
-                    # overflow 체크가 필요한 이유이다. p2.next가 None이면 마지막 노드인거고, p2가 None이면 overflow
-                    if p2 is None:
-                        print("Overflow")
-                        return None
+                # 전체 링크드리스트의 길이보다 k가 더 길면 노답인거다
+                # overflow 체크가 필요한 이유이다. p2.next가 None이면 마지막 노드인거고, p2가 None이면 overflow
+                if p2 is None:
+                    print("Overflow")
+                    return None
 
 
-                    # p2가 마지막 노드일때까지 p1, p2를 이동시킨다.
-                    while p2.next is not None:
-                        p2 = p2.next
-                        p1 = p1.next
+                # p2가 마지막 노드일때까지 p1, p2를 이동시킨다.
+                while p2.next is not None:
+                    p2 = p2.next
+                    p1 = p1.next
 
-                    #p2.next가 None인 순간 p1은 마지막 Node인 p2로부터 k만큼 떨어져 있게 된다.
-                    return p1.val
+                #p2.next가 None인 순간 p1은 마지막 Node인 p2로부터 k만큼 떨어져 있게 된다.
+                return p1.val
 
 class LinkedListTest(unittest.TestCase):
     def test(self):
@@ -70,3 +70,7 @@ class LinkedListTest(unittest.TestCase):
         self.assertEqual(4, ll.kth_element_from_last(5))
         self.assertEqual(3, ll.kth_element_from_last(6))
         self.assertEqual(None, ll.kth_element_from_last(7))
+
+
+
+
